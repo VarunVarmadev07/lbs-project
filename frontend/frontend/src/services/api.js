e2a67f165b1726c48a51c791b9f4c52b6a0bf83b
VarunVarmadev07 <varunvarmadev07@outlook.com>
2026-04-28 13:09:52 +0530

    Add files via upload

---
+++ frontend/frontend/src/services/api.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const API_URL = "https://library-management-system-tl4l.onrender.com/api/books";
+
+export const getBooks = () => axios.get(API_URL);
+export const addBook = (data) => axios.post(API_URL, data);
+export const deleteBook = (id) => axios.delete(`${API_URL}/${id}`);
+export const updateBook = (id, data) =>
+  axios.put(`${API_URL}/${id}`, data);
